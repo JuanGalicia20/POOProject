f2152375fb60e778799f834332b62a5699c8fd59
tujony <70181220+tujony@users.noreply.github.com>
2020-09-01 23:17:38 -0600

    Add files via upload

---
+++ AppPOO/app/src/main/java/com/project/app/poo/Pregunta.java
@@ -0,0 +1,35 @@
+package com.project.poo;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Pregunta {
+    private String name;
+    private Map consejo;
+
+    public Map getConsejo() {
+        return consejo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Pregunta(String name, Map consejo) {
+        this.name = name;
+        this.consejo = consejo;
+    }
+
+    public Pregunta(){
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "Pregunta{" +
+                "name='" + name + '\'' +
+                ", consejo='" + consejo + '\'' +
+                '}';
+    }
+}
