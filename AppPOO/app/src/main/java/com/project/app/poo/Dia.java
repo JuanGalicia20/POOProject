63303ad483b35e5d37f2aa959e176e5b6fea3ebc
JoseDeLaVega <josephdlv@hotmail.com>
2020-09-01 20:37:52 -0600

    Add files via upload
    Clase Dia
    Atributos: 
    --> name: String
    --> listaPlan: ArrayList<String>
    Métodos:
    --> gets
    --> addPlan(String x)

---
+++ AppPOO/app/src/main/java/com/project/app/poo/Dia.java
@@ -0,0 +1,25 @@
+
+
+public class Dia{
+	//Atributos
+	private String name;
+	private ArrayList<String> listaPlan;
+
+	//Constructor
+	public Dia(String name){
+		this.name = name;
+	}
+
+	//get&set
+	public String getName(){
+		return name;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+
+	//añadir plan
+	public void addPlan(String x){
+		listaPlan.add(x);
+	}
+}
