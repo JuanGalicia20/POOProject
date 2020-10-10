2273a3ab4b7a7ddabfc5d4c1efa6ca1a23e46b03
elisamayoa <63657896+elisamayoa@users.noreply.github.com>
2020-10-10 13:55:29 -0600

    Create Periodo.java

---
+++ AplicacionMovil/app/src/main/java/com/project/app/aplicacionmovil/Periodo.java
@@ -0,0 +1,68 @@
+package com.project.app.aplicacionmovil;
+
+
+public class Periodo {
+    private int horaInicial;
+    private int horaFinal;
+    private int minInicial;
+    private int minFinal;
+    private Actividades actividad;
+
+    public Periodo(int horaInicial, int horaFinal, int minInicial, int minFinal, Actividades actividad) {
+        this.horaInicial = horaInicial;
+        this.horaFinal = horaFinal;
+        this.minInicial = minInicial;
+        this.minFinal = minInicial;
+        this.actividad = actividad;
+    }
+
+    public Actividades getActividad() {
+        return actividad;
+    }
+
+    public int getHoraInicial() {
+        return horaInicial;
+    }
+
+    public int getHoraFinal() {
+        return horaFinal;
+    }
+
+    public int getMinInicial() {
+        return minInicial;
+    }
+
+    public int getMinFinal() {
+        return minFinal;
+    }
+
+    public void setActividad(Actividades actividad) {
+        this.actividad = actividad;
+    }
+
+    public void setHoraInicial() {
+        this.horaInicial = horaInicial;
+    }
+
+    public void setHoraFinal() {
+        this.horaFinal = horaFinal;
+    }
+
+    public void setMinInicial() {
+        this.minInicial = minInicial;
+    }
+
+    public void setMinFinal() {
+        this.minFinal = minFinal;
+    }
+
+
+    
+    @Override
+    public String toString() {
+        return "Periodo{" +
+                "actividad='" + actividad + '\'' +
+                ", hora='" + horaInicial + ":"+ minInicial + "-" + horaFinal + ":" + minFinal + '\'' +
+                '}';
+    }
+}
