94da1f1b97b3afee52490db7106927fefe0fb578
elisamayoa <63657896+elisamayoa@users.noreply.github.com>
2020-11-07 03:04:42 -0600

    Create AdaptadorTareas.java
    Creacion de clase para Adaptador de Recycler View

---
+++ AplicacionMovil/app/src/main/java/com/project/app/aplicacionmovil/AdaptadorTareas.java
@@ -0,0 +1,48 @@
+package com.project.app.aplicacionmovil;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+
+public class AdaptadorTareas extends RecyclerView.Adapter<AdaptadorTareas.ViewHolderTareas>{
+
+    private ArrayList<ObjetoTareas> listaTareas;
+    @NonNull
+    @Override
+    public AdaptadorTareas.ViewHolderTareas onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.itemtareas,null,false);
+        RecyclerView.LayoutParams lp = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        view.setLayoutParams(lp);
+        return new ViewHolderTareas(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdaptadorTareas.ViewHolderTareas holder, int position) {
+        holder.tituloTarea.setText(listaTareas.get(position).getTitulo());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaTareas.size();
+    }
+
+    public class ViewHolderTareas extends RecyclerView.ViewHolder
+    {
+        TextView tituloTarea;
+
+        public ViewHolderTareas(@NonNull View itemView) {
+            super(itemView);
+            tituloTarea = (TextView) itemView.findViewById(R.id.textDTarea);
+        }
+    }
+
+
+}
