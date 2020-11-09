27f9272d6d2a2c5e44b7793d80431da1e64e1755
JuanGalicia20 <69065800+JuanGalicia20@users.noreply.github.com>
2020-11-08 20:57:43 -0600

    Correccion de errores de commits
    Se corrigio un error entre los commits realizados

--- AplicacionMovil/app/src/main/java/com/project/app/aplicacionmovil/HorarioSeleccionado.java
+++ AplicacionMovil/app/src/main/java/com/project/app/aplicacionmovil/HorarioSeleccionado.java
@@ -1,5 +1,7 @@
 package com.project.app.aplicacionmovil;
 
+import android.app.Activity;
+import android.app.AuthenticationRequiredException;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
@@ -7,10 +9,19 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.api.Distribution;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
@@ -32,11 +43,14 @@ import org.w3c.dom.Text;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HorarioSeleccionado extends AppCompatActivity {
-
+    private final FirebaseFirestore db1 = FirebaseFirestore.getInstance();
     private ImageView lunes;
     private ImageView martes;
     private ImageView miercoles;
@@ -53,12 +67,14 @@ public class HorarioSeleccionado extends AppCompatActivity {
     private LinearLayout sabadoLl;
     private LinearLayout domingoLl;
     private TextView txtNombre;
+    private TextView txtNomAct;
     private String nombreH;
     private String user;
-    private String[] diasCom={"Lunes","Martes","Miercoles","Jueves","Vriernes","Sabado","Domingo"};
+    private String[] diasCom={"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};
     private String[] diaslv={"Lunes","Martes","Miercoles","Jueves","Viernes"};
     private ImageView crearAct;
 
+    private TextView txtDesc;
     private TextView txtInicio;
     private TextView txtFin;
     private Button iniciar;
@@ -69,8 +85,6 @@ public class HorarioSeleccionado extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-
-
         this.nombreH = intent.getStringExtra("Horario");
         this.user = intent.getStringExtra("User");
         setContentView(R.layout.activity_horario_seleccionado);
@@ -78,6 +92,8 @@ public class HorarioSeleccionado extends AppCompatActivity {
         setSupportActionBar(toolbar);
         CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         toolBarLayout.setTitle(getTitle());
+
+        cargarActividades();
 
         final CardView newAct = (CardView) findViewById(R.id.newactivity);
         newAct.setVisibility(View.GONE);
@@ -97,9 +113,29 @@ public class HorarioSeleccionado extends AppCompatActivity {
                 newAct.setVisibility(View.GONE);
             }
         });
+        final Spinner daySpinner = (Spinner) findViewById(R.id.daySpinner);
+        String length;
 
-        Spinner daySpinner = (Spinner) findViewById(R.id.daySpinner);
-        daySpinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, diasCom));
+        db1.collection("Users").document(user).collection("Horarios").document(nombreH).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists() == true){
+                    sabadoLl = (LinearLayout) findViewById(R.id.linearSabado);
+                    domingoLl = (LinearLayout) findViewById(R.id.linearDomingo);
+
+
+                    if(((String)documentSnapshot.get("length")).equals("Weekend")){
+                        daySpinner.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, diasCom));
+                    }
+                    else if (((String) documentSnapshot.get("length")).equals("Week")){
+                        daySpinner.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, diaslv));
+                        sabadoLl.setVisibility(View.GONE);
+                        domingoLl.setVisibility(View.GONE);
+                    }
+                }
+            }
+        });
+
         daySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
@@ -116,16 +152,9 @@ public class HorarioSeleccionado extends AppCompatActivity {
             }
         });
 
-        crearAct = (ImageView)findViewById(R.id.imageButtonCrearHorario);
-        crearAct.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"Actividad creada correctamente",Toast.LENGTH_SHORT).show();
-                closeKeyboard();
-                newAct.setVisibility(View.GONE);
-            }
-        });
 
+        txtDesc = (TextView)findViewById(R.id.txtDescripcion);
+        txtNomAct = (TextView)findViewById(R.id.txtNomActividad);
         txtNombre=(TextView)findViewById(R.id.nombreH);
         txtNombre.setText(nombreH);
         setTitle(nombreH);
@@ -222,11 +251,13 @@ public class HorarioSeleccionado extends AppCompatActivity {
                                             "hh:mm aa"
                                     );
                                     txtInicio.setText(f12Horas.format(date));
+
                                 } catch (ParseException e) {
                                     e.printStackTrace();
                                 }
                             }
                         },12,0,false
+
                 );
 
                 timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
@@ -267,6 +298,187 @@ public class HorarioSeleccionado extends AppCompatActivity {
                 timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 timePickerDialog.updateTime(t1H,t1M);
                 timePickerDialog.show();
+            }
+        });
+
+        crearAct = (ImageView)findViewById(R.id.imageButtonCrearHorario);
+        crearAct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    String dia = daySpinner.getSelectedItem().toString();
+                    addActividad(dia, txtInicio.getText().toString(), txtFin.getText().toString());
+                    Toast.makeText(getApplicationContext(),"Actividad creada correctamente ",Toast.LENGTH_SHORT).show();
+                    closeKeyboard();
+                    newAct.setVisibility(View.GONE);
+                }
+                catch (Exception e){
+                    Toast.makeText(getApplicationContext(),"Parece que hubo un error, revise los campos e intente nuevamente. ",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+    public void addActividad(String daySpinner, String horaIni, String horaFin ){
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        HashMap<String, String> data = new HashMap<String, String>();
+        data.put("name", txtNomAct.getText().toString());
+        data.put("inicio", horaIni);
+        data.put("final", horaFin);
+        data.put("description", txtDesc.getText().toString());
+
+        db.collection("Users").document(user).collection("Horarios").document(nombreH).collection(daySpinner).document(txtNomAct.getText().toString()).set(data);
+        db.collection("Users").document(user).collection("Horarios").document(nombreH).collection(daySpinner).document("Hora0").delete();
+        finish();
+        startActivity(getIntent());
+
+    }
+    public void cargarActividades(){
+
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        lunesLl = (LinearLayout)findViewById(R.id.nuevosLunes);
+        martesLl = (LinearLayout)findViewById(R.id.nuevosMartes);
+        miercolesLl = (LinearLayout)findViewById(R.id.nuevosMiercoles);
+        juevesLl = (LinearLayout)findViewById(R.id.nuevosJueves);
+        viernesLl = (LinearLayout)findViewById(R.id.nuevosViernes);
+        sabadoLl = (LinearLayout)findViewById(R.id.nuevosSabado);
+        domingoLl = (LinearLayout)findViewById(R.id.nuevosDomingo);
+
+        final ArrayList<LinearLayout> diasLl = new ArrayList<>();
+        diasLl.add(lunesLl);
+        diasLl.add(martesLl);
+        diasLl.add(miercolesLl);
+        diasLl.add(juevesLl);
+        diasLl.add(viernesLl);
+        diasLl.add(sabadoLl);
+        diasLl.add(domingoLl);
+
+        db.collection("Users").document(user).collection("Horarios").document(nombreH).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists() == true){
+
+
+                    if(((String)documentSnapshot.get("length")).equals("Weekend")){
+                        for(int i = 1; i <= 7; i++){
+
+                            final int finalI = i;
+                            db.collection("Users").document(user).collection("Horarios").document(nombreH).collection(diasCom[i-1]).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                                    int j = finalI;
+                                    if(task.isSuccessful()){
+
+                                        for(DocumentSnapshot doc : task.getResult().getDocuments()){
+                                            ArrayList<String> info = new ArrayList<>();
+
+                                            String content;
+
+                                            try{
+                                                info.add(doc.get("name").toString());
+                                                info.add(doc.get("inicio").toString());
+                                                info.add(doc.get("final").toString());
+
+                                                content = "" + info.get(0) + "\nDe: " + info.get(1) + "\t Hasta: " + info.get(2);
+
+                                                Button newButton = new Button(getApplicationContext());
+                                                newButton.setTag(info.get(0));
+                                                newButton.setText(content);
+
+                                                diasLl.get(j-1).addView(newButton);
+                                            }
+                                            catch (java.lang.NullPointerException e){
+                                                content = "Sin registros";
+                                                Button newButton = new Button(getApplicationContext());
+                                                newButton.setTag(content);
+                                                newButton.setText(content);
+
+                                                diasLl.get(j-1).addView(newButton);
+                                            }
+
+
+                                           /*System.out.println(doc.get("name"));
+                                            for (Map.Entry entry : d1.entrySet())
+                                            {
+                                                info.add(entry.getValue().toString());
+                                            }
+                                            String content;
+                                            if(info.size() == 0){
+                                                content = "Sin registros";
+                                                Button newButton = new Button(getApplicationContext());
+                                                newButton.setTag(content);
+                                                newButton.setText(content);
+                                                diasLl.get(j-1).addView(newButton);
+                                            }
+                                            else {
+                                                String[] parts = info.get(0).split("");
+                                                if(parts.length == 9 && parts[8].equals("M") && (parts[1].equals("1") || parts[1].equals("0"))){
+                                                    content = "" + info.get(1) + "\nDe: " + info.get(0) + "\t Hasta: " + info.get(2);
+                                                }
+                                                else{
+                                                    content = "" + info.get(0) + "\nDe: " + info.get(1) + "\t Hasta: " + info.get(3);
+                                                }
+                                                Button newButton = new Button(getApplicationContext());
+                                                newButton.setTag(info.get(0));
+                                                newButton.setText(content);
+                                                diasLl.get(j-1).addView(newButton);
+                                            }*/
+
+                                        }
+                                    }
+                                }
+                            });
+                        }
+                    }
+                    else if (((String) documentSnapshot.get("length")).equals("Week")){
+                        for(int i = 1; i <= diaslv.length; i++){
+
+                            final int finalI = i;
+                            db.collection("Users").document(user).collection("Horarios").document(nombreH).collection(diaslv[i-1]).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                    int j = finalI;
+                                    if(task.isSuccessful()){
+
+                                        for(DocumentSnapshot doc : task.getResult().getDocuments()){
+                                            ArrayList<String> info = new ArrayList<>();
+
+                                            String content;
+                                            try{
+                                                info.add(doc.get("name").toString());
+                                                info.add(doc.get("inicio").toString());
+                                                info.add(doc.get("final").toString());
+
+                                                content = "" + info.get(0) + "\nDe: " + info.get(1) + "\t Hasta: " + info.get(2);
+
+                                                Button newButton = new Button(getApplicationContext());
+                                                newButton.setTag(info.get(0));
+                                                newButton.setText(content);
+
+                                                diasLl.get(j-1).addView(newButton);
+                                            }
+                                            catch (java.lang.NullPointerException e){
+                                                content = "Sin registros";
+                                                Button newButton = new Button(getApplicationContext());
+                                                newButton.setTag(content);
+                                                newButton.setText(content);
+
+                                                diasLl.get(j-1).addView(newButton);
+                                            }
+
+
+                                        }
+                                    }
+                                }
+                            });
+                        }
+                    }
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Parece que ocurrio un error, intenta de nuevo.", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
